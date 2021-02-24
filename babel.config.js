7e752fb59170ce05ee9b5f0246ec8d508e09611d
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-02-24 19:18:17 +0300

    [add] - babel config

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,21 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-class-properties',
+    [
+      'module-resolver',
+      {
+        root: [ './src' ],
+        alias: {
+          '/': './',
+        },
+        stripExtensions: [
+          '.js',
+          '.vue',
+        ],
+      },
+    ],
+  ],
 };
