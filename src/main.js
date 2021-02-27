6e8ac4a27c368f1ab8fb656708025b4fe5ecd868
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-02-27 13:09:25 +0300

    [add] import scss files to main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
+import 'normalize.css';
+
+import '@/assets/styles/main.scss';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+console.log(process.env);
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
