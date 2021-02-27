6bde7468ed75a0e09441b9f4543328b93d392a4e
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-02-27 13:11:42 +0300

    [add] app level header & footer components

---
+++ src/common/components/AppFooter/AppFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    <section class="footer__content container">
+      <slot name="footerTitle">
+        <p class="footer__title">
+          {{ title }}
+        </p>
+      </slot>
+
+      <slot name="footerContent" />
+    </section>
+  </footer>
+</template>
+
+<script>
+const DEFAULT_TITLE = 'footer';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: DEFAULT_TITLE,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.footer__title {
+  text-transform: uppercase;
+  letter-spacing: 1.37px;
+}
+</style>
---
+++ src/common/components/AppHeader/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="appHeader ">
+    <section class="appHeader__content container">
+      <router-link to="/">
+        <h1 class="appHeader__title">
+          brand
+        </h1>
+      </router-link>
+      <slot />
+    </section>
+  </header>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
+
+<style lang="scss">
+.appHeader {
+  background-color: $backgroundColor--light;
+
+  .appHeader__title {
+    font-size: 20px;
+  }
+
+  .appHeader__content {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    height: 60px;
+
+    text-transform: uppercase;
+  }
+}
+
+
+</style>
