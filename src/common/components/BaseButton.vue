33126397e5012a79e7258f7fa573bb4b84b8b1d5
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-02-27 13:11:18 +0300

    [add] base components

---
+++ src/common/components/BaseButton.vue
@@ -0,0 +1,101 @@
+<template>
+  <button
+    class="button"
+    :class="classes"
+    @click="click"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    click: {
+      type: Function,
+      required: true,
+    },
+    icon: {
+      type: Boolean,
+      default: false,
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    const { icon, primary, round } = this;
+
+    return {
+      classes: {
+        'button--round': round,
+        'button--primary': primary,
+        icon,
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.icon {
+  width: 20px;
+  height: 20px;
+
+  transition: .3s fill;
+
+  fill: $buttonColor--primary;
+}
+
+.button {
+  cursor: pointer;
+  transition: .3s border, .3s color, .3s background-color;
+
+  border: 1px solid $buttonColor--primary;
+  border-radius: 100px;
+  background-color: $buttonFontColor--primary;
+}
+
+.button--round {
+  display: block;
+
+  border-radius: 50%;
+}
+
+.button--primary {
+  padding: 13px;
+
+  letter-spacing: 1.39px;
+  text-transform: uppercase;
+
+  font-family: 'Lato-Bold', sans-serif;
+  font-size: 13px;
+
+  &:hover {
+    color: $buttonFontColor--primary;
+    border: 1px solid $buttonColor--hover;
+    background-color: $buttonColor--hover;
+  }
+}
+
+
+/* */
+.button--wishlist:hover {
+  border: 1px solid $buttonColor--hover;
+}
+
+.button--wishlist:hover > .icon {
+  fill: $buttonColor--hover;
+}
+
+.button--in-cart {
+  pointer-events: none;
+
+  background-color: $buttonColor--primary;
+}
+</style>
---
+++ src/common/components/BaseLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="baseLayout"
+    :class="{ container }"
+  >
+    <app-header class="baseLayout__header">
+      <slot name="headerContent" />
+    </app-header>
+
+    <main class="baseLayout__main">
+      <div class="container">
+        <slot name="mainContent" />
+      </div>
+    </main>
+
+    <app-footer class="baseLayout__footer">
+      <template #footerContent>
+        <slot name="footerContent" />
+      </template>
+    </app-footer>
+
+    <slot name="default" />
+  </div>
+</template>
+
+<script>
+import AppFooter from '@/common/components/AppFooter/AppFooter.vue';
+import AppHeader from '@/common/components/AppHeader/AppHeader.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  props: {
+    container: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.baseLayout {
+  height: 100%;
+
+  display: flex;
+  flex-direction: column
+}
+
+.baseLayout__header,
+.baseLayout__footer {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+
+  height: 60px;
+
+  text-transform: uppercase;
+
+  background-color: $backgroundColor--light;
+}
+
+.baseLayout__main {
+  display: flex;
+  flex: 1 0 auto;
+  flex-direction: row;
+
+  margin: 30px 0 20px;
+}
+
+
+</style>
