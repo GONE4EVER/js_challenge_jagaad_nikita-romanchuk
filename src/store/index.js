7b1e8ce066a920227dc62d7061f3a35939ce1053
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-03-01 01:27:47 +0300

    [add] import of activity module to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import venuesModule, { venuesModuleName } from '@/domains/Venue/store';
-
+import activitiesModule from 'domains/Activity/store';
+import { activitiesModuleName } from 'domains/Activity/store/constants';
+import { subscribeToVenueCurrentIdChange } from 'domains/Activity/store/plugins';
+import venuesModule from 'domains/Venue/store';
+import { venuesModuleName } from 'domains/Venue/store/constants';
 
 Vue.use(Vuex);
 Vue.config.devtools = true;
 
 const store = new Vuex.Store({
+  plugins: [ subscribeToVenueCurrentIdChange ],
   modules: {
     [venuesModuleName]: venuesModule,
+    [activitiesModuleName]: activitiesModule,
   },
 });
 
@@ -17,9 +22,8 @@ store.subscribeAction({
   before: action => {
     console.log(`before action ${action.type}`);
   },
-  after: (action, state) => {
+  after: action => {
     console.log(`after action ${action.type}`);
-    console.log({ state });
   },
 });
 
