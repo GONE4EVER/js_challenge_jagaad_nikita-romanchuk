858f634ff261d72bdd75c7ebc5a3eba2eb49186d
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-03-03 16:20:18 +0300

    [fix] add eslint disable maxlen for icon components

--- src/common/components/Icons/ArrowRightIcon.vue
+++ src/common/components/Icons/ArrowRightIcon.vue
@@ -10,13 +10,15 @@
     <!-- Generator: Sketch 42 (36781) - http://www.bohemiancoding.com/sketch -->
     <desc>Created with Sketch.</desc>
     <defs />
+    <!-- eslint-disable max-len -->
     <polygon
       id="Left-Iocn"
       stroke="none"
       :fill="fillColor"
       fill-rule="evenodd"
-      :points="points"
+      points="22.9427745 22.9999999 19 26.9458774 20.0541226 28 25.0541226 23 20.0541226 18 19 19.0541226"
     />
+    <!-- eslint-enable max-len -->
   </svg>
 </template>
 
@@ -47,10 +49,6 @@ export default {
       default: DEFAULT_WIDTH,
     },
   },
-  data: () => ({
-    points: `22.9427745 22.9999999 19 26.9458774 20.0541226
-     28 25.0541226 23 20.0541226 18 19 19.0541226`,
-  }),
 };
 </script>
 
