97bc81f5bd7e8d783a0a5c3259ce6c5e0f86ade8
Nikita Romanchuk <nromanchuk1997@gmail.com>
2021-03-07 18:56:12 +0300

    [add] jest config props

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/domains/**/*.vue',
+    '<rootDir>/views/**/*.vue',
+  ],
+  testMatch: [
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
 };
+
